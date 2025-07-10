a01cc0215138b5e6b2421040b31b208a509436ba
tracyuniverse <thetracycastro@gmail.com>
2025-07-10 13:07:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 from flask import Flask, render_template, request, jsonify
-import os
-from utils import load_model, predict_image
-from werkzeug.utils import secure_filename
 from PIL import Image
+import torch
+import torchvision.transforms as transforms
+import os
+from utils import load_model, predict_gesture
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-
-model, transform, class_names = load_model('gesture_model.pth')
+model = load_model('gesture_model.pth')
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['image']
-    filename = secure_filename(file.filename)
-    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(path)
-
-    image = Image.open(path).convert("RGB")
-    prediction = predict_image(model, image, transform, class_names)
-
-    return jsonify({'prediction': prediction})
+    if file:
+        filepath = os.path.join('static', 'uploaded.png')
+        file.save(filepath)
+        prediction = predict_gesture(filepath, model)
+        return jsonify({'gesture': prediction})
+    return jsonify({'error': 'No file received'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
