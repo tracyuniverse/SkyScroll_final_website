7b9e88e594b6cee873751e6d8524ab35e993305e
tracyuniverse <thetracycastro@gmail.com>
2025-07-10 15:02:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,20 @@ else:
 
 # 🔽 THEN load the model
 model = load_model(model_path)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['image']
+    if file:
+        filepath = os.path.join('static', 'uploaded.png')
+        file.save(filepath)
+        prediction = predict_gesture(filepath, model)
+        return jsonify({'gesture': prediction})
+    return jsonify({'error': 'No file received'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
